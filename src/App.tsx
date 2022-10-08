69690453ae9d50fc923439f223e2f67ad76f8477
Justine Kizhakkinedath <justine@kizhak.com>
2022-10-09 00:10:15 +0530

    feat: Add basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import type { Component } from "solid-js"
+import { lazy } from "solid-js"
+import { Routes, Route } from "@solidjs/router"
+
+const Homepage = lazy(() => import("./pages/home"))
+const ProjectDetails = lazy(() => import("./pages/project-details"))
+const AboutMe = lazy(() => import("./pages/about-me"))
+const ContactMe = lazy(() => import("./pages/contact-me"))
 
 const App: Component = () => {
-  return <div>Hello World</div>
+  return (
+    <Routes>
+      <Route path="/" component={Homepage} />
+      <Route path="/projects/:project-id" component={ProjectDetails} />
+      <Route path="/about-me" component={AboutMe} />
+      <Route path="/contact-me" component={ContactMe} />
+    </Routes>
+  )
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,17 @@
 /* @refresh reload */
 import { render } from "solid-js/web"
+import { Router } from "@solidjs/router"
 
 import "virtual:windi-devtools"
 import "virtual:windi.css"
 import "./index.css"
 import App from "./App"
 
-render(() => <App />, document.getElementById("root") as HTMLElement)
+render(
+  () => (
+    <Router>
+      <App />
+    </Router>
+  ),
+  document.getElementById("root") as HTMLElement
+)
