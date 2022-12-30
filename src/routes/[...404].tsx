492536e9483ecedf0e0282d5ce05d0f539350378
Justine Kizhakkinedath <justine@kizhak.com>
2022-12-31 01:34:48 +0530

    feat: add solidjs files

---
+++ src/routes/[...404].tsx
@@ -0,0 +1,8 @@
+
+export default function NotFound() {
+  return (
+    <main class="">
+      You seems to be lost!
+    </main>
+  );
+}
---
+++ src/routes/index.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main class="">
+      Hi.. I'm Justine Kizhakkinedath and I like to make awesome digital experiences!!
+    </main>
+  );
+}
