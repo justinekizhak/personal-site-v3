bf07eb6abaa34ecd657cff20b26198855d27893f
Justine Kizhakkinedath <justine@kizhak.com>
2023-01-01 20:01:38 +0530

    feat: update footer contents

--- src/modules/footer/content.tsx
+++ src/modules/footer/content.tsx
@@ -1,14 +1,51 @@
-import { A } from "solid-start";
+import { For } from "solid-js";
+import { A, useLocation } from "solid-start";
+
+interface LinkObject {
+  href: string;
+  text: string;
+}
+
+const links: LinkObject[] = [
+  {
+    href: "/about-me",
+    text: "About Me",
+  },
+  {
+    href: "/my-projects",
+    text: "My Projects",
+  },
+];
+
+const backLink: LinkObject = {
+  href: "/",
+  text: "Back to Home",
+};
 
 export default function Footer() {
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  const getLinkObject = (link: LinkObject): LinkObject => {
+    const linkHref = link.href;
+    if (linkHref === currentPath) {
+      return backLink;
+    }
+    return link;
+  };
+
   return (
     <footer class="absolute bottom-0 flex justify-between inset-x-0">
-      <A href="/about-me" class="mx-4 my-2">
-        About Me
-      </A>
-      <A href="/my-projects" class="mx-4 my-2">
-        My Projects
-      </A>
+      <For each={links}>
+        {(link: LinkObject) => {
+          const newLink = getLinkObject(link);
+          return (
+            <A href={newLink.href} class="mx-4 my-2">
+              {newLink.text}
+            </A>
+          );
+        }}
+      </For>
     </footer>
   );
 }
